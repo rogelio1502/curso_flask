d71eb3cfa491a73fc16f4e042ae1071ce28610f2
rogelio1502 <55726045+rogelio1502@users.noreply.github.com>
2020-09-17 22:51:23 -0500

    Update config.py

--- ejemplos/u32/aplicacion/config.py
+++ ejemplos/u32/aplicacion/config.py
@@ -4,5 +4,5 @@ SECRET_KEY = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
 PWD = os.path.abspath(os.curdir)
 
 DEBUG = True
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}/dbase.db'.format(PWD)
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://rogelio1502:susana15@rogelio1502.mysql.pythonanywhere-services.com/rogelio1502$tienda'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
